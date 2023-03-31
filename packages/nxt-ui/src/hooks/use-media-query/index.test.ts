753f391c32083ec964e663677884a90cc52e3579
Huijie Wei <huijiewei@outlook.com>
2023-03-31 16:54:29 +0800

    test(ui): add use-media-query test

---
+++ packages/nxt-ui/src/hooks/use-media-query/index.test.ts
@@ -0,0 +1,47 @@
+import { useMediaQuery } from './index';
+import { MatchMediaMock, testRenderHook } from '@resolid/nxt-tests';
+import { describe, test, expect, beforeEach, afterEach } from 'vitest';
+
+describe('useMediaQuery', () => {
+  let matchMedia: MatchMediaMock;
+
+  beforeEach(() => {
+    matchMedia = new MatchMediaMock();
+  });
+
+  afterEach(() => {
+    matchMedia.clear();
+  });
+
+  test('should return true when the media query matches', () => {
+    const query = '(min-width: 500px)';
+
+    matchMedia.useMediaQuery(query);
+
+    const { result } = testRenderHook(() => useMediaQuery(query));
+
+    expect(result.current).toBe(true);
+  });
+
+  test('should return false when the media query does not match', () => {
+    const query = '(max-width: 1000px)';
+
+    matchMedia.useMediaQuery('(max-width: 500px)');
+
+    const { result } = testRenderHook(() => useMediaQuery(query));
+
+    expect(result.current).toBe(false);
+  });
+
+  test('should update the result when the media query changes', () => {
+    matchMedia.useMediaQuery('(max-width: 500px)');
+
+    const query = '(min-width: 768px)';
+
+    matchMedia.useMediaQuery(query);
+
+    const { result } = testRenderHook(() => useMediaQuery(query));
+
+    expect(result.current).toBe(true);
+  });
+});
