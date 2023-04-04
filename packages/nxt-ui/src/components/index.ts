b6679f2832bb56d6620b62f10af5a0f4bb2c34dc
Huijie Wei <huijiewei@outlook.com>
2023-04-04 13:05:05 +0800

    feat(ui): add createContext utility

--- packages/nxt-ui/src/components/index.ts
+++ packages/nxt-ui/src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './visually-hidden/VisuallyHidden';
 export * from './icon/Icon';
+
+export * from '../utils/context';
---
+++ packages/nxt-ui/src/utils/context.ts
@@ -0,0 +1,40 @@
+import { type Context, createContext as ReactCreateContext, type Provider, useContext as ReactUseContext } from 'react';
+
+export type CreateContextOptions = {
+  name: string;
+  strict?: boolean;
+  errorMessage?: string;
+};
+
+type CreateContextReturn<T> = [Provider<T>, () => T, Context<T>];
+
+export const createContext = <ContextValueType>(options: CreateContextOptions) => {
+  const { name, strict = true, errorMessage } = options;
+
+  const Context = ReactCreateContext<ContextValueType | undefined>(undefined);
+
+  Context.displayName = name;
+
+  const useContext = () => {
+    const context = ReactUseContext(Context);
+
+    if (context == undefined && strict) {
+      const error = new Error(
+        errorMessage ||
+          `use${name.replace(
+            'Context',
+            ''
+          )} returned \`undefined\`. Seems you forgot to wrap component within ${name.replace('Context', 'Provider')}`
+      );
+
+      error.name = 'ContextError';
+      Error.captureStackTrace?.(error, useContext);
+
+      throw error;
+    }
+
+    return context;
+  };
+
+  return [Context.Provider, useContext, Context] as CreateContextReturn<ContextValueType>;
+};
