56b2cd0956411414a902b273e3d3ce714dbff32b
Huijie Wei <huijiewei@outlook.com>
2023-04-14 09:36:51 +0800

    feat(ui): add polymorphic component

---
+++ packages/nxt-ui/src/primitives/polymorphic/index.ts
@@ -0,0 +1,68 @@
+import type { Assign, Merge } from '@resolid/nxt-utils';
+import type {
+  ComponentProps,
+  ComponentPropsWithRef,
+  ComponentPropsWithoutRef,
+  ElementType,
+  ForwardRefRenderFunction,
+  ReactElement,
+  ValidationMap,
+  WeakValidationMap,
+} from 'react';
+import { forwardRef } from 'react';
+
+type PropsOf<T extends ElementType> = ComponentPropsWithoutRef<T> & {
+  as?: ElementType;
+};
+
+export type PrimitiveProps<
+  Type extends ElementType = ElementType,
+  Props extends Record<string, unknown> = Record<never, never>
+> = Merge<ComponentPropsWithoutRef<Type>, Props>;
+
+export const primitiveComponent = <
+  C extends ElementType = ElementType,
+  Props extends Record<string, unknown> = Record<never, never>
+>(
+  render: (
+    props: Merge<ComponentPropsWithoutRef<C>, Props>,
+    ref?: ComponentPropsWithRef<C>['ref']
+  ) => ReactElement | null
+) => {
+  return forwardRef(render);
+};
+
+export type PolymorphicProps<
+  Default extends ElementType,
+  Component extends ElementType,
+  PermanentProps extends Record<never, never>,
+  DefaultProps extends Record<never, never>,
+  ComponentProps extends Record<never, never>
+> =
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  any extends Component
+    ?
+        | Assign<DefaultProps, PermanentProps & { as?: Default | ElementType }>
+        | Assign<ComponentProps, PermanentProps & { as?: Component }>
+    : never;
+
+export type PolymorphicComponent<
+  Component extends ElementType,
+  Props extends Record<never, never> = Record<never, never>
+> = {
+  <AsComponent extends ElementType = Component>(
+    props: PolymorphicProps<Component, AsComponent, Props, ComponentProps<Component>, ComponentProps<AsComponent>>
+  ): JSX.Element;
+
+  displayName?: string;
+  propTypes?: WeakValidationMap<unknown>;
+  contextTypes?: ValidationMap<unknown>;
+  id?: string;
+};
+
+export const polymorphicComponent = <
+  Component extends ElementType,
+  Props extends Record<never, never> = Record<never, never>
+>(
+  component: ForwardRefRenderFunction<never, Assign<PropsOf<Component>, Props> & { as?: ElementType }>
+) => forwardRef(component) as unknown as PolymorphicComponent<Component, Props>;
