6aed5e0407ee286c0575ad2df5b833f1f6598578
Huijie Wei <huijiewei@outlook.com>
2023-04-15 20:43:31 +0800

    fix(website): move viewport meta tag to html, fix PSI report problem

--- website/src/root.tsx
+++ website/src/root.tsx
@@ -9,10 +9,8 @@ export default function Root() {
     <>
       <ColorModeScript />
       <Helmet prioritizeSeoTags defaultTitle={'Resolid Nxt'} titleTemplate={'%s - Resolid Nxt'}>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Get your app up and running with React" />
         <meta name="keywords" content="react, react-router, tailwindcss, vite, typescript, framework" />
-        <meta name="theme-color" content="#4586be" />
       </Helmet>
       <NxtProvider>
         <Suspense fallback={<LazyLoader />}>
