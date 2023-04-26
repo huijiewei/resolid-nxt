2f681dbf7721019fecf91abe914767cb74e6c613
Huijie Wei <huijiewei@outlook.com>
2023-04-27 07:59:21 +0800

    refactor(utils): change promise wait return type

--- packages/nxt-utils/src/promise/index.ts
+++ packages/nxt-utils/src/promise/index.ts
@@ -1,4 +1,4 @@
-export const wait = (delay: number): Promise<null> => {
+export const wait = (delay: number): Promise<void> => {
   return new Promise((resolve) => {
     setTimeout(resolve, delay);
   });
