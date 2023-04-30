4805bb2b7c12e3a95956928106104df1883c98a8
Huijie Wei <huijiewei@outlook.com>
2023-04-30 15:44:43 +0800

    feat(website): add CopyButton component

---
+++ website/src/common/components/CopyButton.tsx
@@ -0,0 +1,26 @@
+import { Tooltip, useClipboard } from '@resolid/nxt-ui';
+import { __DEV__ } from '@resolid/nxt-utils';
+import { Clipboard } from '~/common/icons/Clipboard';
+import { ClipboardCheck } from '~/common/icons/ClipboardCheck';
+
+export const CopyButton = ({ content }: { content: string }) => {
+  const { copied, onCopy } = useClipboard();
+
+  return (
+    <div className={'absolute right-1.5 top-1.5'}>
+      <Tooltip color={copied ? 'success' : undefined} content={copied ? 'Copied' : 'Copy Code'}>
+        <button type={'button'} className={'relative appearance-none p-1 font-medium'} onClick={() => onCopy(content)}>
+          {copied ? (
+            <ClipboardCheck size={'1em'} className={'text-fg-success'} />
+          ) : (
+            <Clipboard size={'1em'} className={'text-fg-muted hover:text-link-hovered'} />
+          )}
+        </button>
+      </Tooltip>
+    </div>
+  );
+};
+
+if (__DEV__) {
+  CopyButton.displayName = 'CopyButton';
+}
