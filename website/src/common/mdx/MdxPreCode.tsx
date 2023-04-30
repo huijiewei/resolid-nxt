294b15e9b5a6db333197e947b929cf4a33b4e335
Huijie Wei <huijiewei@outlook.com>
2023-04-30 15:46:09 +0800

    chore(website): add CopyButton to code block

--- website/src/common/mdx/MdxPreCode.tsx
+++ website/src/common/mdx/MdxPreCode.tsx
@@ -1,17 +1,23 @@
 import type { Language } from '@resolid/nxt-prism';
-import type { ComponentProps } from 'react';
+import type { PrimitiveProps } from '@resolid/nxt-ui';
 import { CodeHighlight } from '~/common/components/CodeHighlight';
+import { CopyButton } from '~/common/components/CopyButton';
 
-export const MdxCode = (props: ComponentProps<'code'>) => {
+type MdxCodeProps = {
+  children: string;
+};
+
+export const MdxCode = (props: PrimitiveProps<'code', MdxCodeProps>) => {
   const { className, children } = props;
 
   return className ? (
-    <div className={'rounded mt-6 border overflow-x-auto scrollbar scrollbar-thin'}>
+    <div className={'relative rounded mt-6 border overflow-x-auto scrollbar scrollbar-thin'}>
       <CodeHighlight
         className={'p-3'}
         language={(className.replace('language-', '') as Language) || 'jsx'}
-        code={children as string}
+        code={children}
       />
+      <CopyButton content={children} />
     </div>
   ) : (
     <code className={'m-0 px-[0.4em] py-[0.2em] bg-bg-subtle rounded text-sm'}>{children}</code>
--- website/src/modules/ui/components/DemoExample.tsx
+++ website/src/modules/ui/components/DemoExample.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react';
 import { CodeHighlight } from '~/common/components/CodeHighlight';
+import { CopyButton } from '~/common/components/CopyButton';
 
 type DemoExampleProps = {
   preview: () => ReactNode;
@@ -10,11 +11,14 @@ export const DemoExample = ({ preview, snippet }: DemoExampleProps) => {
   return (
     <div className={'my-4 border rounded'}>
       <div className={'overflow-x-auto scrollbar scrollbar-thin p-3'}>{preview()}</div>
-      <CodeHighlight
-        className={'rounded p-3 border-t overflow-x-auto scrollbar scrollbar-thin'}
-        language={'jsx'}
-        code={snippet}
-      />
+      <div className={'relative rounded'}>
+        <CodeHighlight
+          className={'p-3 border-t overflow-x-auto scrollbar scrollbar-thin'}
+          language={'jsx'}
+          code={snippet}
+        />
+        <CopyButton content={snippet} />
+      </div>
     </div>
   );
 };
