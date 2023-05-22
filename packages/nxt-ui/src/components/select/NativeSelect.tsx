b215084bfc770f879dbf1e58b8bfd0dda822edd6
Huijie Wei <huijiewei@outlook.com>
2023-05-22 11:37:00 +0800

    refactor(ui): optimize NativeSelect

--- packages/nxt-ui/src/components/select/NativeSelect.tsx
+++ packages/nxt-ui/src/components/select/NativeSelect.tsx
@@ -1,6 +1,7 @@
-import { cx } from '@resolid/nxt-utils';
+import { __DEV__, cx } from '@resolid/nxt-utils';
 import { primitiveComponent } from '../../primitives';
 import type { Size } from '../../utils/types';
+import { SelectChevron } from './SelectChevron';
 
 export type NativeSelectProps = {
   /**
@@ -53,24 +54,16 @@ export const NativeSelect = primitiveComponent<'select', NativeSelectProps>((pro
       </select>
       <div
         className={cx(
-          'pointer-events-none absolute top-0 bottom-0 right-0 flex items-center justify-center text-gray-500',
+          'pointer-events-none absolute top-0 bottom-0 right-0 flex items-center justify-center',
           sizeStyle.chevron
         )}
       >
-        <svg
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          xmlns="http://www.w3.org/2000/svg"
-          width={'1em'}
-          height={'1em'}
-          viewBox="0 0 24 24"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
-        </svg>
+        <SelectChevron />
       </div>
     </div>
   );
 });
 
-NativeSelect.displayName = 'NativeSelect';
+if (__DEV__) {
+  NativeSelect.displayName = 'NativeSelect';
+}
