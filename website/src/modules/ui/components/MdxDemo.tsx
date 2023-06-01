48644072e3b3b6b8a9c26cd5d825a3484460af3d
Huijie Wei <huijiewei@outlook.com>
2023-06-02 00:28:24 +0800

    fix(website): fix show Loader on MdxDemo unload status

--- website/src/modules/ui/components/MdxDemo.tsx
+++ website/src/modules/ui/components/MdxDemo.tsx
@@ -1,14 +1,18 @@
-import { type ComponentType } from 'react';
+import { useMemo, type ComponentType } from 'react';
 import { LazyLoader } from '~/common/components/LazyLoader';
 import { dynamicLoader } from '~/common/dynamic/dynamicLoader';
 
 const demos = import.meta.glob<boolean, string, { [key: string]: ComponentType }>('../demos/**/*.tsx');
 
 export const MdxDemo = ({ file, demo, height }: { file: string; demo: string; height?: number }) => {
-  const Demo = dynamicLoader({
-    loader: async () => ({ default: (await demos[`../demos/${file}`]())[demo] }),
-    fallback: <LazyLoader height={height} />,
-  });
+  const Demo = useMemo(
+    () =>
+      dynamicLoader({
+        loader: async () => ({ default: (await demos[`../demos/${file}`]())[demo] }),
+        fallback: <LazyLoader height={height} />,
+      }),
+    [demo, file, height]
+  );
 
   return <Demo />;
 };
