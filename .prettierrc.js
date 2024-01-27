168ee3a51583836d254df7e9add20eb1b730f18c
Huijie Wei <huijiewei@outlook.com>
2024-01-27 12:28:51 +0800

    chore: dependencies update

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
-module.exports = {
+// noinspection JSUnusedGlobalSymbols
+export default {
   singleQuote: true,
   printWidth: 120,
   tabWidth: 2,
   tailwindConfig: './website/tailwind.config.cjs',
+  tailwindFunctions: ['cx', 'cva'],
   plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
 };
